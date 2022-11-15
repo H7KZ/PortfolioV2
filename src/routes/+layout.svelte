90143b38e756b27e8426a1005be575341e7b3665
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-11-15 10:17:03 +0100

    import css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import "$css/tailwind.css";
+	import "$css/scrollbar.css";
+	import "$css/gradients.css";
 	import Navbar from '$components/common/navbar/Navbar.svelte';
 </script>
 
