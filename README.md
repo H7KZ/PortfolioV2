114251abdd633f2f0c0d11f7c68a1a5d03247b84
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-12-14 18:04:43 +0100

    hotfix: spelling mistakes, fiverr icon

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 [![wakatime](https://wakatime.com/badge/github/H7KZ/Portfolio.svg)](https://wakatime.com/badge/github/H7KZ/Portfolio)
 
-### Written in SvelteKit using TypeScript and CSS Framework TailwindCSS.
+- ## Written in SvelteKit using TypeScript and CSS Framework TailwindCSS.
 
-## You are allowed to get inspired and copy some things I have no problem with that
+---
 
-### Although if you could put me to credits - thanks
+## You are allowed to get inspired and copy few things, I have no problem with that, but you need to give me credits or ask me for permission.
 
-![kominekjan cz_](https://user-images.githubusercontent.com/74021016/204525255-2fb27ad3-9020-4973-95b5-df3418d0682a.png)
+---
+
+![kominekjan cz_](https://user-images.githubusercontent.com/74021016/204525255-2fb27ad3-9020-4973-95b5-df3418d0682a.png) \* outdated design
--- src/content/AboutContent.ts
+++ src/content/AboutContent.ts
@@ -8,12 +8,12 @@ export default {
 			{
 				title: 'In general',
 				description:
-					'Hello, my name is <b>Jan Komínek</b> and I am an <b>open-minded entrepreneur that focuses on frontend web development</b>. In parallel I am <b>studying industrial high school</b> in the <b>field of cyber security</b>.'
+					'Hello, my name is <b>Jan Komínek</b> and I am an <b>open-minded entrepreneur that focuses on frontend web development</b>. In parallel I am <b>studying at industrial high school a field of cyber security</b>.'
 			},
 			{
 				title: 'Frontend development',
 				description:
-					'<b>My interest in web development started in 2020</b>, since then I have learned every aspect of frontend web development and I have been <b>constantly learning new technologies</b>. I have a <b>great experience in HTML, CSS, TailwindCSS, JavaScript, TypeScript, SvelteKit and VueJS</b>.'
+					'<b>My interest in web development started in 2020</b>, since then I have learned every aspect of frontend web development and I have been <b>constantly learning new technologies</b>. I have a <b>lot experience in HTML, CSS, TailwindCSS, JavaScript, TypeScript, SvelteKit and VueJS</b>.'
 			},
 			{
 				title: 'Learn more',
